e4afe27a75459b29be5cc57cba284c02a9fec0e2
sgandeplli <sgandepalli@softility.com>
2025-03-25 14:35:26 +0530

    Create var.tf

---
+++ var.tf
@@ -0,0 +1,18 @@
+variable "aws_region" {
+  type = string
+}
+variable "aws_vpc" {
+  type = string
+}
+variable "eks_cluster_role" {
+  type = string
+}
+variable "eks_cluster_name" {
+  type = string
+}
+variable "eks_node_role" {
+  type = string
+}
+variable "eks_nodegroup_name" {
+  type = string
+}
